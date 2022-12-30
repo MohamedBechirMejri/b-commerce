53b67f44f744ec7d51bc784fd55b15d1c7cb5156
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-30 10:06:50 +0100

    Add cart placeholder

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,33 +2,36 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 
 import Account from "./Account";
-import Cart from "./Cart";
+import CartToggle from "./CartToggle";
 import Logo from "./Logo";
 import Nav from "./Nav";
 import NavToggle from "./NavToggle";
 import Search from "./Search";
+import Cart from "./Cart";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
     <div className="relative h-screen w-full overflow-hidden">
       <Nav isOpen={isNavOpen} setIsOpen={setIsNavOpen} />
       <motion.div
         initial={{ x: 0 }}
-        animate={{ x: isNavOpen ? "20rem" : 0 }}
+        animate={{ x: isSidebarOpen ? "-20rem" : isNavOpen ? "20rem" : 0 }}
         transition={{ type: "spring", damping: 27, stiffness: 150 }}
-        className="absolute z-10 h-full w-full overflow-y-scroll bg-slate-100 elevation-4"
+        className="absolute z-10 h-full w-full bg-slate-100 elevation-4"
       >
-        <div className="grid h-24 grid-cols-[1fr,2fr,15fr,1fr,1fr]">
+        <div className="grid h-[5rem] grid-cols-[1fr,2fr,15fr,1fr,1fr]">
           <NavToggle isOpen={isNavOpen} setIsOpen={setIsNavOpen} />
           <Logo />
           <Search />
-          <Cart />
+          <CartToggle isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
           <Account />
         </div>
-        <div className="h-[calc(100vh-6rem)]">{children}</div>
+        <div className="h-[calc(100vh-5rem)]">{children}</div>
       </motion.div>
+      <Cart isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
     </div>
   );
 };
