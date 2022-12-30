d72089accd9d5b94289c7c62b893249015ff4287
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-30 14:10:56 +0100

    add submenu

--- src/components/Layout/Nav/Nav.tsx
+++ src/components/Layout/Nav/Nav.tsx
@@ -1,23 +1,28 @@
 import { motion } from "framer-motion";
-import Link from "next/link";
+import { useState } from "react";
+import type Category from "../../../types/Category";
 import NavMenu from "./NavMenu";
 
 const categories = Array(10).fill({
+  id: 0,
   name: "Home",
   url: "/",
-  subcategories: [
+  subCategories: [
     {
+      id: 1,
       name: "Home",
       url: "/",
-      subcategories: [
+      subCategories: [
         {
+          id: 2,
           name: "Home",
           url: "/",
-          subcategories: [
+          subCategories: [
             {
+              id: 3,
               name: "Home",
               url: "/",
-              subcategories: [{ name: "Home", url: "/" }],
+              subCategories: [],
             },
           ],
         },
@@ -33,21 +38,33 @@ const Nav = ({
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
 }) => {
+  const [submenu, setSubMenu] = useState<Category[] | null>(null);
   return (
     <motion.nav
       initial={{ x: "-20rem" }}
       animate={{ x: isOpen ? 0 : "-20rem" }}
       transition={{ type: "spring", damping: 27, stiffness: 150 }}
-      className="absolute top-0 z-50 flex h-screen w-[20rem] select-none flex-col gap-8 bg-slate-900 p-8 text-5xl font-bold text-slate-100"
+      className="absolute top-0 z-50 flex h-screen w-[20rem] select-none flex-col overflow-scroll bg-slate-900 py-8 text-3xl font-bold text-slate-100 scrollbar-none"
+      onMouseLeave={() => setSubMenu(null)}
     >
-      {categories.map((category) => (
-        <NavMenu
-          key={category.name}
-          category={category}
-          isOpen={isOpen}
-          setIsOpen={setIsOpen}
-        />
-      ))}
+      {categories.map((category) => {
+        return (
+          <NavMenu
+            key={category.name} // TODO: replace with id
+            category={category}
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            submenu={submenu}
+            setSubMenu={setSubMenu}
+          />
+        );
+      })}
+      <motion.div
+        initial={{ scaleX: 0 }}
+        animate={{ scaleX: submenu ? 1 : 0 }}
+        transition={{ type: "spring", damping: 27, stiffness: 150 }}
+        className="fixed top-0 left-[20rem] h-screen w-[35rem] origin-left bg-slate-800"
+      ></motion.div>
     </motion.nav>
   );
 };
--- src/components/Layout/Nav/NavMenu.tsx
+++ src/components/Layout/Nav/NavMenu.tsx
@@ -1,38 +1,45 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { useState } from "react";
+import type Category from "../../../types/Category";
 
 const NavMenu = ({
   category,
   isOpen,
   setIsOpen,
+  submenu,
+  setSubMenu,
 }: {
-  category: {
-    name: string;
-    url: string;
-  };
+  category: Category;
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
+  submenu: Category[] | null;
+  setSubMenu: (submenu: Category[] | null) => void;
 }) => {
-  const [isSubmenuVisible, setIsSubmenuVisible] = useState(true);
+  const handleMouseEnter = () => {
+    setSubMenu(category.subCategories || null);
+  };
 
   return (
     <motion.div
       initial={{ y: 15, scale: 1 }}
       animate={{ y: isOpen ? 0 : 550, scale: isOpen ? 1 : 0 }}
+      className="relative flex w-full items-center justify-center"
       transition={{ type: "spring", damping: 27, stiffness: 150 }}
-      className="relative flex items-center justify-center rounded-3xl"
-      onMouseEnter={() => setIsSubmenuVisible(true)}
-      onMouseLeave={() => setIsSubmenuVisible(false)}
+      onMouseEnter={handleMouseEnter}
     >
-      <Link href={category.url} onClick={() => setIsOpen(false)}>
+      <Link
+        href={"/categories/" + category.id}
+        onClick={() => setIsOpen(false)}
+        style={{
+          backgroundColor:
+            submenu && submenu.toString() === category.subCategories?.toString()
+              ? "#ffffff27"
+              : "transparent",
+        }}
+        className="block h-full w-full p-6 text-center transition-all"
+      >
         {category.name}
       </Link>
-      <motion.div
-        initial={{ scaleX: 0 }}
-        animate={{ scaleX: isSubmenuVisible ? 1 : 0 }}
-        className="absolute left-full h-screen w-[25rem] bg-red-500"
-      ></motion.div>
     </motion.div>
   );
 };
