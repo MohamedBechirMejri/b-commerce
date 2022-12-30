b7441edc6578232cc9dddcb0b378c8951f630a49
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-30 11:58:39 +0100

    Add Menu Hamburger

--- src/components/Layout/Nav/NavToggle.tsx
+++ src/components/Layout/Nav/NavToggle.tsx
@@ -1,4 +1,20 @@
+import type { SVGMotionProps } from "framer-motion";
 import { motion } from "framer-motion";
+import type { RefAttributes } from "react";
+
+const Path = (
+  props: JSX.IntrinsicAttributes &
+    SVGMotionProps<SVGPathElement> &
+    RefAttributes<SVGPathElement>
+) => (
+  <motion.path
+    fill="transparent"
+    strokeWidth="1"
+    stroke="hsl(0, 0%, 18%)"
+    strokeLinecap="round"
+    {...props}
+  />
+);
 
 const NavToggle = ({
   isOpen,
@@ -8,17 +24,15 @@ const NavToggle = ({
   setIsOpen: (isOpen: boolean) => void;
 }) => {
   return (
-    <div className="flex h-full items-center justify-center">
+    <motion.div
+      initial={false}
+      animate={isOpen ? "open" : "closed"}
+      className="flex h-full items-center justify-center"
+    >
       <motion.button
-        initial={{ scale: 0, x: 0, y: 25, backgroundColor: "#0f172a" }}
-        animate={{
-          scale: 1,
-          y: 0,
-          backgroundColor: isOpen ? "#f1f5f9" : "#0f172a",
-        }}
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        className="z-20 h-[3.5rem] w-[3.5rem] rounded-full elevation-8"
+        initial={{ scale: 0, y: 25 }}
+        animate={{ scale: 1, y: 0 }}
+        className="relative z-20 flex h-full w-full flex-col items-center justify-center gap-2"
         aria-label="toggle nav"
         aria-pressed="false"
         onClick={(e) => {
@@ -30,8 +44,31 @@ const NavToggle = ({
 
           setIsOpen(!isOpen);
         }}
-      />
-    </div>
+      >
+        <svg width="23" height="23" viewBox="0 0 23 23" className="scale-[1.7]">
+          <Path
+            variants={{
+              closed: { d: "M 2 2.5 L 20 2.5" },
+              open: { d: "M 3 16.5 L 17 2.5" },
+            }}
+          />
+          <Path
+            d="M 2 9.423 L 20 9.423"
+            variants={{
+              closed: { opacity: 1 },
+              open: { opacity: 0 },
+            }}
+            transition={{ duration: 0.1 }}
+          />
+          <Path
+            variants={{
+              closed: { d: "M 2 16.346 L 20 16.346" },
+              open: { d: "M 3 2.5 L 17 16.346" },
+            }}
+          />
+        </svg>
+      </motion.button>
+    </motion.div>
   );
 };
 
