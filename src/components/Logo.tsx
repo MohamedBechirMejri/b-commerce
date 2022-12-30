221053e7ccd7f31882fb51e2cafeca48b4f0f12c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-30 09:37:26 +0100

    position toggle

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,9 @@
 const Logo = () => {
-  return <div className=""></div>;
+  return (
+    <div className="flex h-full items-center justify-center text-2xl font-bold">
+      B Commerce
+    </div>
+  );
 };
 
 export default Logo;
--- src/components/NavToggle.tsx
+++ src/components/NavToggle.tsx
@@ -8,28 +8,30 @@ const NavToggle = ({
   setIsOpen: (isOpen: boolean) => void;
 }) => {
   return (
-    <motion.button
-      initial={{ scale: 0, x: "-50%", y: 25, backgroundColor: "#0f172a" }}
-      animate={{
-        scale: 1,
-        y: 0,
-        backgroundColor: isOpen ? "#f1f5f9" : "#0f172a",
-      }}
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }}
-      className="z-20 h-[3.5rem] w-[3.5rem] rounded-full elevation-8"
-      aria-label="toggle nav"
-      aria-pressed="false"
-      onClick={(e) => {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        const target: HTMLButtonElement = e.target;
-        const pressed = target.getAttribute("aria-pressed") === "true";
-        target.setAttribute("aria-pressed", `${!pressed}`);
+    <div className="flex h-full items-center justify-center">
+      <motion.button
+        initial={{ scale: 0, x: 0, y: 25, backgroundColor: "#0f172a" }}
+        animate={{
+          scale: 1,
+          y: 0,
+          backgroundColor: isOpen ? "#f1f5f9" : "#0f172a",
+        }}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="z-20 h-[3.5rem] w-[3.5rem] rounded-full elevation-8"
+        aria-label="toggle nav"
+        aria-pressed="false"
+        onClick={(e) => {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          const target: HTMLButtonElement = e.target;
+          const pressed = target.getAttribute("aria-pressed") === "true";
+          target.setAttribute("aria-pressed", `${!pressed}`);
 
-        setIsOpen(!isOpen);
-      }}
-    />
+          setIsOpen(!isOpen);
+        }}
+      />
+    </div>
   );
 };
 
