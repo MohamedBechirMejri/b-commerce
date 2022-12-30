c55c4e9bc647bc23f61a42d0d42f3e9df457dc97
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-30 09:37:40 +0100

    clean code

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,40 +9,26 @@ import NavToggle from "./NavToggle";
 import Search from "./Search";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  // return (
-  //   <>
-  //     <div className="grid h-24 grid-cols-[2fr,2fr,15fr,1fr,1fr]">
-  //       <Logo />
-  //       <div></div>
-  //       <Search />
-  //       <Cart />
-  //       <Account />
-  //     </div>
-  //     {children}
-  //   </>
-  // );
-
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
     <div className="relative h-screen w-full overflow-hidden">
-      {/* Body */}
+      <Nav isOpen={isNavOpen} setIsOpen={setIsNavOpen} />
       <motion.div
         initial={{ x: 0 }}
         animate={{ x: isNavOpen ? "20rem" : 0 }}
         transition={{ type: "spring", damping: 27, stiffness: 150 }}
-        className="absolute z-10 h-full w-full overflow-scroll bg-slate-100 elevation-4 scrollbar-none selection:bg-slate-900 selection:text-slate-100"
+        className="absolute z-10 h-full w-full overflow-y-scroll bg-slate-100 elevation-4"
       >
-        <div className="grid h-24 grid-cols-[2fr,2fr,15fr,1fr,1fr]">
-          <Logo />
+        <div className="grid h-24 grid-cols-[1fr,2fr,15fr,1fr,1fr]">
           <NavToggle isOpen={isNavOpen} setIsOpen={setIsNavOpen} />
+          <Logo />
           <Search />
           <Cart />
           <Account />
         </div>
-        {children}
+        <div className="h-[calc(100vh-6rem)]">{children}</div>
       </motion.div>
-      <Nav isOpen={isNavOpen} setIsOpen={setIsNavOpen} />
     </div>
   );
 };
