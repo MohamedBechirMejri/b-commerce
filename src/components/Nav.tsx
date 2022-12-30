e76f4f76dabd180fb516d9a0e04a1ffcc65cdb03
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-30 08:50:02 +0100

    add nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,38 @@
-import Account from "./Account";
-import Cart from "./Cart";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
-const className = " h-full w-full border border-current";
+const links = [
+  { name: "Home", url: "/" },
+  { name: "Create", url: "/new" },
+  { name: "Contact", url: "/contact" },
+  { name: "Lab", url: "https://lab.mohamedbechirmejri.dev" },
+];
 
-const Nav = () => {
+const Nav = ({
+  isOpen,
+  setIsOpen,
+}: {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+}) => {
   return (
-    <nav className={"grid grid-cols-[15fr,1fr,1fr] border-0" + className}>
-      <div className={"bg-green-500x0 " + className}></div>
-      <Cart />
-      <Account />
+    <nav className="absolute top-0 h-screen w-[20rem] select-none flex-col bg-slate-900 p-8 text-5xl font-bold text-slate-100">
+      {links.map((link) => (
+        <motion.div
+          key={link.name + link.name}
+          initial={{ y: 15, scale: 1 }}
+          animate={{
+            y: isOpen ? 0 : 550,
+            scale: isOpen ? 1 : 0,
+          }}
+          transition={{ type: "spring", damping: 27, stiffness: 150 }}
+          className="flex items-center justify-center rounded-3xl text-5xl"
+        >
+          <Link href={link.url} onClick={() => setIsOpen(false)}>
+            {link.name}
+          </Link>
+        </motion.div>
+      ))}
     </nav>
   );
 };
