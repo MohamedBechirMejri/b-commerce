6a07d71ad95de84f7e91d730b8c7515276c56fcb
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-30 06:47:30 +0100

    style search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,18 @@
-const className = " h-full w-full border border-current";
+import { CgSearch } from "react-icons/cg";
 
 const Search = () => {
-  return <div className={"bg-green-500x0 " + className}></div>;
+  return (
+    <div className="flex h-full w-full">
+      <input
+        type="search"
+        placeholder="Search"
+        className="h-full w-full border-0"
+      />
+      <button className="flex w-24 items-center justify-center text-3xl">
+        <CgSearch />
+      </button>
+    </div>
+  );
 };
 
 export default Search;
