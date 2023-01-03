84a285b3cdca5f9502f6460cd00954babadba0fb
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-03 19:07:56 +0100

    add products placeholder

--- src/components/Browse/Main.tsx
+++ src/components/Browse/Main.tsx
@@ -1,5 +1,100 @@
+import Image from "next/image";
+
+const products = [
+  {
+    id: "1",
+    name: "Product 1",
+    price: 10,
+    image: "https://picsum.photos/500",
+  },
+  {
+    id: "2",
+    name: "Product 2",
+    price: 20,
+    image: "https://picsum.photos/500",
+  },
+  {
+    id: "3",
+    name: "Product 3",
+    price: 30,
+    image: "https://picsum.photos/500",
+  },
+  {
+    id: "4",
+    name: "Product 4",
+    price: 40,
+    image: "https://picsum.photos/500",
+  },
+  {
+    id: "5",
+    name: "Product 5",
+    price: 50,
+    image: "https://picsum.photos/500",
+  },
+  {
+    id: "6",
+    name: "Product 6",
+    price: 60,
+    image: "https://picsum.photos/500",
+  },
+  {
+    id: "7",
+    name: "Product 7",
+    price: 70,
+    image: "https://picsum.photos/500",
+  },
+  {
+    id: "8",
+    name: "Product 8",
+    price: 80,
+    image: "https://picsum.photos/500",
+  },
+  {
+    id: "9",
+    name: "Product 9",
+    price: 90,
+    image: "https://picsum.photos/500",
+  },
+  {
+    id: "10",
+    name: "Product 10",
+    price: 100,
+    image: "https://picsum.photos/500",
+  },
+];
+
 const Main = () => {
-  return <main className=""></main>;
+  return (
+    <main className="grid grid-cols-5 gap-4 overflow-y-scroll p-4 scrollbar-none">
+      {products.map((product) => (
+        <div
+          key={product.id}
+          className="relative h-max overflow-hidden border bg-white"
+        >
+          <Image
+            src={product.image}
+            width={500}
+            height={500}
+            alt={"product"}
+            className="h-[17rem]"
+          />
+          <div className="flex justify-between p-4">
+            <h2>{product.name}</h2>
+            <p>${product.price}</p>
+          </div>
+
+          <div className="grid grid-cols-2 gap-2 p-4">
+            <button className="rounded-md bg-black p-2 text-white">
+              Add to cart
+            </button>
+            <button className="rounded-md bg-black p-2 text-white">
+              Add to wishlist
+            </button>
+          </div>
+        </div>
+      ))}
+    </main>
+  );
 };
 
 export default Main;
