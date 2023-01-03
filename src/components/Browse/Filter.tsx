272ad6c3bd16193ccc0b641e21d763120f520628
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-03 14:42:56 +0100

    add filters placeholders

--- src/components/Browse/Filter.tsx
+++ src/components/Browse/Filter.tsx
@@ -1,5 +1,47 @@
 const Filter = () => {
-  return <div className="border border-current"></div>;
+  const category = {
+    tagCollections: [
+      {
+        name: "sesdredry",
+        tags: [
+          {
+            name: "setsetse",
+          },
+          {
+            name: "tesetst",
+          },
+        ],
+      },
+      {
+        name: "tsets",
+        tags: [
+          {
+            name: "test",
+          },
+        ],
+      },
+    ],
+  };
+
+  return (
+    <div className="p-4 py-8 ">
+      <h1 className="text-2xl">Filter</h1>
+      <ul>
+        {category.tagCollections.map((tagCollection) => (
+          <li key={tagCollection.name}>
+            <h2>{tagCollection.name}</h2>
+            <ul>
+              {tagCollection.tags.map((tag) => (
+                <li key={tag.name}>
+                  <button>{tag.name}</button>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Filter;
