8d31863aca290041b308a5efa65a796e50e93ad3
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-05 11:29:58 +0100

    add nav

---
+++ src/components/Dashboard/Nav.tsx
@@ -0,0 +1,25 @@
+const Nav = () => {
+  return (
+    <nav className="">
+      <ul>
+        <li>
+          <button>Dashboard</button>
+        </li>
+        <li>
+          <button>Products</button>
+        </li>
+        <li>
+          <button>Orders</button>
+        </li>
+        <li>
+          <button>Customers</button>
+        </li>
+        <li>
+          <button>Reports</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
+import Main from "../../components/Dashboard/Main";
+import Nav from "../../components/Dashboard/Nav";
+
 const Dashboard = () => {
-  return <div>test</div>;
+  return (
+    <div className="grid h-screen grid-cols-10 grid-rows-[3rem,1fr]">
+      <header className="col-span-10 flex items-center justify-between p-4 font-bold">
+        <Link href="/">B Commerce</Link>
+        <div className="h-8 w-8 rounded-full bg-slate-300"></div>
+      </header>
+      <Nav />
+      <main className="col-span-9">
+        <Main />
+      </main>
+    </div>
+  );
 };
 
 export default Dashboard;
