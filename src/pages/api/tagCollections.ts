a4fad1c6871a8ecb822b633b4c3f893433b672af
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-07 18:29:35 +0100

    add more methods

--- src/pages/api/tagCollections.ts
+++ src/pages/api/tagCollections.ts
@@ -2,28 +2,35 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 
 import { prisma } from "../../server/db/client";
 
+// TODO: Add Validation and Error Handling and Sanitization and Authorization...
+
 const tagCollections = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { id, name, tags, categoryId } = req.body;
+  const data = { name, tags, categoryId };
+
   if (req.method === "GET") {
     const tagCollections = await prisma.tagCollection.findMany();
     return res.status(200).json(tagCollections);
   }
 
   if (req.method === "POST") {
-    const { body } = req;
+    const tagCollection = await prisma.tagCollection.create({ data });
+    return res.status(201).json(tagCollection);
+  }
 
-    // TODO: Add Validation and Error Handling and Sanitization and Authorization...
-
-    const data = {
-      name: body.name,
-      tags: body.tags,
-      categoryId: body.categoryId,
-    };
-
-    const tagCollection = await prisma.tagCollection.create({
+  if (req.method === "PUT") {
+    const tagCollection = await prisma.tagCollection.update({
+      where: { id },
       data,
     });
+    return res.status(200).json(tagCollection);
+  }
 
-    return res.status(201).json(tagCollection);
+  if (req.method === "DELETE") {
+    const tagCollection = await prisma.tagCollection.delete({ where: { id } });
+    return res
+      .status(200)
+      .json({ message: `Tag Collection ${tagCollection.name} deleted.` });
   }
 };
 
