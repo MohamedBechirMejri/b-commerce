653cdaa716e164c1a109d9b4837cbb4b0d783ba5
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-09 00:35:56 +0100

    try connecting categories, tags and related products

--- src/pages/api/products/index.ts
+++ src/pages/api/products/index.ts
@@ -1,4 +1,5 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
+import generatePrismaConnects from "../../../lib/generatePrismaConnects";
 
 import { prisma } from "../../../server/db/client";
 
@@ -35,9 +36,9 @@ const products = async (req: NextApiRequest, res: NextApiResponse) => {
       stock,
       discount_price,
       availability,
-      related_products,
-      categories,
-      tags,
+      related_products: generatePrismaConnects(related_products),
+      categories: generatePrismaConnects(categories),
+      tags: generatePrismaConnects(tags),
       description,
       technical_sheet,
       images,
