b8dbc710d2f1e2ee915e28c512324600aa60ea34
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-10 02:12:02 +0100

    get products from api

--- src/components/Browse/Main.tsx
+++ src/components/Browse/Main.tsx
@@ -1,76 +1,27 @@
-import Image from "next/image";
+import { type Product } from "@prisma/client";
 
-const products = [
-  {
-    id: "1",
-    name: "Product 1",
-    price: 10,
-    image: "https://picsum.photos/1000",
-  },
-  {
-    id: "2",
-    name: "Product 2",
-    price: 20,
-    image: "https://picsum.photos/2500",
-  },
-  {
-    id: "3",
-    name: "Product 3",
-    price: 30,
-    image: "https://picsum.photos/3000",
-  },
-  {
-    id: "4",
-    name: "Product 4",
-    price: 40,
-    image: "https://picsum.photos/2000",
-  },
-  {
-    id: "5",
-    name: "Product 5",
-    price: 50,
-    image: "https://picsum.photos/1200",
-  },
-  {
-    id: "6",
-    name: "Product 6",
-    price: 60,
-    image: "https://picsum.photos/1500",
-  },
-  {
-    id: "7",
-    name: "Product 7",
-    price: 70,
-    image: "https://picsum.photos/600",
-  },
-  {
-    id: "8",
-    name: "Product 8",
-    price: 80,
-    image: "https://picsum.photos/800",
-  },
-  {
-    id: "9",
-    name: "Product 9",
-    price: 90,
-    image: "https://picsum.photos/900",
-  },
-  {
-    id: "10",
-    name: "Product 10",
-    price: 100,
-    image: "https://picsum.photos/4000",
-  },
-];
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Main = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    axios.get("/api/products").then((res) => setProducts(res.data));
+  }, []);
+
+  useEffect(() => {
+    console.log(products);
+  }, [products]);
+
   return (
     <main className="grid min-h-full gap-4 overflow-y-scroll p-4 scrollbar-none xl:grid-cols-5">
       {products.map((product) => (
         <div
           key={product.id}
-          className="group relative overflow-hidden border border-current bg-cover bg-center"
-          style={{ backgroundImage: `url(${product.image})` }}
+          className="group relative h-[24rem] overflow-hidden border border-current bg-cover bg-center"
+          style={{ backgroundImage: `url(${product.images[0]})` }}
         >
           <div className="absolute bottom-0 w-full translate-y-full bg-white bg-opacity-30 backdrop-blur-3xl transition-all group-hover:translate-y-0">
             <div className="flex justify-between p-4">
