d160f364cdd0ca65f89562fe17a5b9563a39148f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-11 20:50:27 +0100

    update rootlayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,16 @@
 import "~/styles/globals.css";
 
 export const metadata = {
-  title: "MBM Lab",
-  description: "MBM Lab",
+  title: "B-Commerce",
+  description: "The best e-commerce site for all your needs",
   image: "/logo.png",
-  siteUrl: "https://mbm-lab.vercel.app",
+  siteUrl: "https://bcommerce.vercel.app",
 };
 
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <nav className="noscroll fixed left-0 top-1/2 z-50 flex h-[70vh] -translate-x-[93%] -translate-y-1/2 flex-col items-center gap-4 overflow-y-scroll rounded rounded-r-none border-2 bg-[#ffffff00] px-12 shadow ring ring-white backdrop-blur-3xl transition-all duration-300 hover:translate-x-0 hover:p-2 "></nav>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
