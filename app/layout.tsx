05f94b5ece1edc400901f4440b1a4ce980ee3661
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-13 20:39:47 +0100

    add lato font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Lato } from "next/font/google";
+
 import "~/styles/globals.css";
 
 export const metadata = {
@@ -7,10 +9,12 @@ export const metadata = {
   siteUrl: "https://bcommerce.vercel.app",
 };
 
+const lato = Lato({ weight: ["400", "700"], subsets: ["latin"] });
+
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={lato.style}>{children}</body>
     </html>
   );
 }
