e1364a31ed605f9e11b91b3ffabaec76d86020df
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-16 19:10:36 +0100

    add products page

---
+++ app/admin/products/ProductLink.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Link from "next/link";
+import { IoEye, IoPencil, IoTrash } from "react-icons/io5";
+
+export default function ProductLink({
+  product,
+}: {
+  product: {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    stock: number;
+  };
+}) {
+  const deleteProduct = (id: number) => {
+    console.log("Deleting product with id: ", id);
+  };
+
+  return (
+    <tr
+      key={product.id}
+      className="border-b border-gray-600 text-sm text-gray-500"
+    >
+      <td className="px-4 py-3">{product.name}</td>
+      <td className="px-4 py-3">{product.description}</td>
+      <td className="px-4 py-3">{product.price}</td>
+      <td className="px-4 py-3">{product.stock}</td>
+      <td className="flex gap-4 px-4 py-3">
+        <Link
+          href={`/admin/products/${product.id}`}
+          className="text-blue-600 hover:text-blue-400"
+        >
+          <IoEye />
+        </Link>
+        <Link
+          href={`/admin/products/${product.id}/edit`}
+          className="text-yellow-600 hover:text-yellow-400"
+        >
+          <IoPencil />
+        </Link>
+        <button
+          className="text-red-600 hover:text-red-400"
+          onClick={() => {
+            if (confirm("Are you sure?")) {
+              deleteProduct(product.id);
+            }
+          }}
+        >
+          <IoTrash />
+        </button>
+      </td>
+    </tr>
+  );
+}
---
+++ app/admin/products/page.tsx
@@ -0,0 +1,62 @@
+import Link from "next/link";
+import { IoAddCircleOutline } from "react-icons/io5";
+import ProductLink from "./ProductLink";
+
+export default function () {
+  const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      description: "This is product 1",
+      price: 100,
+      stock: 10,
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      description: "This is product 2",
+      price: 200,
+      stock: 20,
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      description: "This is product 3",
+      price: 300,
+      stock: 30,
+    },
+  ];
+
+  return (
+    <div className="h-full overflow-y-scroll">
+      <div className="flex items-center justify-between px-8 py-6">
+        <h1 className="text-2xl font-bold">Products</h1>
+        <Link
+          href="/admin/products/new"
+          className="flex items-center justify-center gap-2 rounded-md bg-black px-4 py-2 text-sm font-semibold text-white hover:bg-gray-800"
+        >
+          <IoAddCircleOutline />
+          <span>New Product</span>
+        </Link>
+      </div>
+      <div className="px-8 py-6">
+        <table className="w-full">
+          <thead>
+            <tr className="border-b border-gray-600 text-left text-sm font-semibold uppercase tracking-wide text-gray-500">
+              <th className="px-4 py-3">Name</th>
+              <th className="px-4 py-3">Description</th>
+              <th className="px-4 py-3">Price</th>
+              <th className="px-4 py-3">Stock</th>
+              <th className="px-4 py-3">Actions</th>
+            </tr>
+          </thead>
+          <tbody className="bg-gray-900">
+            {products.map((product) => (
+              <ProductLink product={product} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
