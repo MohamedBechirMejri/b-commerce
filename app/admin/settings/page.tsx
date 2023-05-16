afc76f21407fdf8e810fbf02336076148a53b7c3
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-16 21:51:56 +0100

    add settings page

---
+++ app/admin/settings/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function () {
+  return (
+    <>
+      <div className="flex items-center justify-between px-8 py-6">
+        <h1 className="text-2xl font-bold">Settings</h1>
+      </div>
+      <div className="grid place-items-center px-8 py-6">Soon...</div>
+    </>
+  );
+}
