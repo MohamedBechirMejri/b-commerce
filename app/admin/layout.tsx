d53b1b45d73cc0d38a2ce1cfb43f8133fa73caaf
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-17 23:35:08 +0100

    fix height

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,7 +2,7 @@ import Nav from "./Nav";
 
 export default function ({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid min-h-[100svh] min-w-[100svw] grid-cols-[auto,1fr] grid-rows-1 border-[#222] bg-[#141414] text-white">
+    <div className="grid h-[100svh] grid-cols-[auto,1fr] grid-rows-1 border-[#222] bg-[#141414] text-white">
       <Nav />
       <main className="h-full overflow-y-scroll">{children}</main>
     </div>
