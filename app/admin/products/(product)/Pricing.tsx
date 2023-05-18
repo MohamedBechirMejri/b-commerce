4fb11b75075c8664fbaf945566b9981dd6c99156
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-18 06:54:55 +0100

    add pricing tab

--- app/admin/products/(product)/Pricing.tsx
+++ app/admin/products/(product)/Pricing.tsx
@@ -1,3 +1,43 @@
 export default function Pricing() {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-2 auto-rows-min">
+      <div className="flex p-4 flex-col">
+        <label className="text-sm font-semibold text-gray-600" htmlFor="price">
+          Price
+        </label>
+        <input
+          type="number"
+          className="border border-gray-300 rounded-md px-4 py-2 mt-2"
+          id="price"
+          min={0}
+        />
+      </div>
+
+      <div className="flex p-4 flex-col">
+        <label
+          className="text-sm font-semibold text-gray-600"
+          htmlFor="salePrice"
+        >
+          Sale Price
+        </label>
+        <input
+          type="number"
+          className="border border-gray-300 rounded-md px-4 py-2 mt-2"
+          id="salePrice"
+          min={0}
+        />
+      </div>
+
+      <div className="flex p-4 flex-col">
+        <label className="text-sm font-semibold text-gray-600" htmlFor="onSale">
+          On Sale
+        </label>
+        <input
+          type="checkbox"
+          className="border border-gray-300 rounded-md px-4 py-2 mt-2"
+          id="onSale"
+        />
+      </div>
+    </div>
+  );
 }
