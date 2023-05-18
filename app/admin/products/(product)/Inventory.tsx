53c29d54b7c6e39e01ea90b0908cfd53a4b98033
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-18 07:15:28 +0100

    add inventory tab

--- app/admin/products/(product)/Inventory.tsx
+++ app/admin/products/(product)/Inventory.tsx
@@ -1,3 +1,62 @@
 export default function Inventory() {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-2 auto-rows-min">
+      <div className="flex p-4 flex-col">
+        <label className="text-sm font-semibold text-gray-600" htmlFor="stock">
+          Stock
+        </label>
+        <input
+          type="number"
+          className="border border-gray-300 rounded-md px-4 py-2 mt-2"
+          id="stock"
+          min={0}
+        />
+      </div>
+
+      <div className="flex p-4 flex-col">
+        <label
+          className="text-sm font-semibold text-gray-600"
+          htmlFor="restockAlert"
+        >
+          Restock alert (-1 means no alert)
+        </label>
+        <input
+          type="number"
+          className="border border-gray-300 rounded-md px-4 py-2 mt-2"
+          id="minStock"
+          min={-1}
+        />
+      </div>
+
+      <div className="flex p-4 flex-col">
+        <label
+          className="text-sm font-semibold text-gray-600"
+          htmlFor="minAmount"
+        >
+          Minimum amount
+        </label>
+        <input
+          type="number"
+          className="border border-gray-300 rounded-md px-4 py-2 mt-2"
+          id="minAmount"
+          min={1}
+        />
+      </div>
+
+      <div className="flex p-4 flex-col">
+        <label
+          className="text-sm font-semibold text-gray-600"
+          htmlFor="maxAmount"
+        >
+          Maximum amount
+        </label>
+        <input
+          type="number"
+          className="border border-gray-300 rounded-md px-4 py-2 mt-2"
+          id="maxAmount"
+          min={1}
+        />
+      </div>
+    </div>
+  );
 }
