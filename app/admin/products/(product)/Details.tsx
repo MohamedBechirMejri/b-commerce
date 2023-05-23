d7edb583d0a94753728de33e887a1a0b18ab697e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-23 12:04:54 +0100

    refactor details

--- app/admin/products/(product)/Details.tsx
+++ app/admin/products/(product)/Details.tsx
@@ -1,89 +1,71 @@
 export default function Details() {
+  // Define an array of objects with the properties for each input field
+  const fields = [
+    { id: "name", label: "Name", type: "text" },
+    { id: "reference", label: "Reference", type: "text" },
+    { id: "description", label: "Description", type: "textarea", rows: 4 },
+    {
+      id: "categories",
+      label: "Categories",
+      type: "select",
+      multiple: true,
+      options: [
+        { value: "1", text: "Category 1" },
+        { value: "2", text: "Category 2" },
+        { value: "3", text: "Category 3" },
+      ],
+    },
+    {
+      id: "brand",
+      label: "Brand",
+      type: "select",
+      options: [
+        { value: "1", text: "Brand 1" },
+        { value: "2", text: "Brand 2" },
+        { value: "3", text: "Brand 3" },
+      ],
+    },
+    { id: "published", label: "Published", type: "checkbox" },
+  ];
+
+  // Use map to iterate over the array and render each input field
   return (
     <div className="grid grid-cols-2 auto-rows-min">
-      <div className="flex p-4 flex-col">
-        <label className="text-sm font-semibold text-gray-600" htmlFor="name">
-          Name
-        </label>
-        <input
-          type="text"
-          className="border border-gray-300 rounded-md px-4 py-2 mt-2"
-          id="name"
-        />
-      </div>
-
-      <div className="flex p-4 flex-col">
-        <label
-          className="text-sm font-semibold text-gray-600"
-          htmlFor="reference"
-        >
-          Reference
-        </label>
-        <input
-          type="text"
-          className="border border-gray-300 rounded-md px-4 py-2 mt-2"
-          id="reference"
-        />
-      </div>
-
-      <div className="flex p-4 flex-col">
-        <label
-          className="text-sm font-semibold text-gray-600"
-          htmlFor="description"
-        >
-          Description
-        </label>
-        <textarea
-          className="border border-gray-300 rounded-md px-4 py-2 mt-2"
-          rows={4}
-          id="description"
-        />
-      </div>
-
-      <div className="flex p-4 flex-col">
-        <label
-          className="text-sm font-semibold text-gray-600"
-          htmlFor="categories"
-        >
-          Categories
-        </label>
-        <select
-          className="border border-gray-300 rounded-md px-4 py-2 mt-2"
-          id="categories"
-          multiple
-        >
-          <option value="1">Category 1</option>
-          <option value="2">Category 2</option>
-          <option value="3">Category 3</option>
-        </select>
-      </div>
-      <div className="flex p-4 flex-col">
-        <label className="text-sm font-semibold text-gray-600" htmlFor="brand">
-          Brand
-        </label>
-        <select
-          className="border border-gray-300 rounded-md px-4 py-2 mt-2"
-          id="brand"
-        >
-          <option value="1">Brand 1</option>
-          <option value="2">Brand 2</option>
-          <option value="3">Brand 3</option>
-        </select>
-      </div>
-
-      <div className="flex p-4 flex-col">
-        <label
-          className="text-sm font-semibold text-gray-600"
-          htmlFor="published"
-        >
-          Published
-        </label>
-        <input
-          type="checkbox"
-          className="border border-gray-300 rounded-md px-4 py-2 mt-2"
-          id="published"
-        />
-      </div>
+      {fields.map(field => (
+        <div className="flex flex-col p-4" key={field.id}>
+          <label
+            className="text-sm font-semibold text-gray-600"
+            htmlFor={field.id}
+          >
+            {field.label}
+          </label>
+          {field.type === "textarea" ? (
+            <textarea
+              className="px-4 py-2 mt-2 border border-gray-300 rounded-md"
+              rows={field.rows}
+              id={field.id}
+            />
+          ) : field.type === "select" ? (
+            <select
+              className="px-4 py-2 mt-2 border border-gray-300 rounded-md"
+              id={field.id}
+              multiple={field.multiple}
+            >
+              {field.options!.map(option => (
+                <option value={option.value} key={option.value}>
+                  {option.text}
+                </option>
+              ))}
+            </select>
+          ) : (
+            <input
+              type={field.type}
+              className="px-4 py-2 mt-2 border border-gray-300 rounded-md"
+              id={field.id}
+            />
+          )}
+        </div>
+      ))}
     </div>
   );
 }
