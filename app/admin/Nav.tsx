06bd408a14b05b41e6546be0e70b99cf2fef81ac
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-23 12:14:23 +0100

    refactor nav

--- app/admin/Nav.tsx
+++ app/admin/Nav.tsx
@@ -18,6 +18,16 @@ const isActive = (route: string) => {
 };
 
 export default function Nav() {
+  // Define an array of objects with the properties for each link
+  const links = [
+    { href: "/admin", icon: <IoStatsChart />, text: "Overview" },
+    { href: "/admin/products", icon: <TbShoppingBag />, text: "Products" },
+    { href: "/admin/orders", icon: <TbTruckDelivery />, text: "Orders" },
+    { href: "/admin/customers", icon: <TbUser />, text: "Customers" },
+    { href: "/admin/settings", icon: <TbSettings2 />, text: "Settings" },
+  ];
+
+  // Use map to iterate over the array and render each link
   return (
     <nav className="py-8 text-gray-400">
       <div className="grid h-full w-[22svw] max-w-[14rem] grid-rows-[auto,1fr,auto] border-r border-[#222] pl-8">
@@ -35,57 +45,21 @@ export default function Nav() {
         </Link>
 
         <div className="flex flex-col rounded-br-3xl rounded-tr-3xl border-b border-t border-[#222] py-6">
-          <Link
-            href={"/admin"}
-            className={
-              "flex items-center justify-start gap-3 py-3 transition-all hover:text-white " +
-              isActive("/admin")
-            }
-          >
-            <IoStatsChart /> Overview
-          </Link>
-          <Link
-            href={"/admin/products"}
-            className={
-              "flex items-center justify-start gap-3 py-3 transition-all hover:text-white " +
-              isActive("/admin/products")
-            }
-          >
-            <TbShoppingBag /> Products
-          </Link>
-          <Link
-            href={"/admin/orders"}
-            className={
-              "flex items-center justify-start gap-3 py-3 transition-all hover:text-white " +
-              isActive("/admin/orders")
-            }
-          >
-            <TbTruckDelivery /> Orders
-          </Link>
-          <Link
-            href={"/admin/customers"}
-            className={
-              "flex items-center justify-start gap-3 py-3 transition-all hover:text-white " +
-              isActive("/admin/customers")
-            }
-          >
-            <TbUser /> Customers
-          </Link>
-          <Link
-            href={"/admin/settings"}
-            className={
-              "flex items-center justify-start gap-3 py-3 transition-all hover:text-white " +
-              isActive("/admin/settings")
-            }
-          >
-            <TbSettings2 /> Settings
-          </Link>
+          {links.map(link => (
+            <Link
+              href={link.href}
+              className={
+                "flex items-center justify-start gap-3 py-3 transition-all hover:text-white " +
+                isActive(link.href)
+              }
+              key={link.href}
+            >
+              {link.icon} {link.text}
+            </Link>
+          ))}
         </div>
 
-        <div
-          className="flex flex-col rounded-tr-3xl border-t border-[#222]
-            py-4"
-        >
+        <div className="flex flex-col rounded-tr-3xl border-t border-[#222] py-4">
           <a
             href="https://bcommerce.com"
             target="_blank"
