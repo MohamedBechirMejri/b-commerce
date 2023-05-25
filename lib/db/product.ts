978948767fa5af0f9d0340d0d1070bcced79ee72
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-25 03:28:05 +0100

    add rest of crud ops to products

--- lib/db/product.ts
+++ lib/db/product.ts
@@ -1,3 +1,15 @@
 import prisma from "./prisma";
 
 export const getAllProducts = async () => await prisma.product.findMany();
+
+export const getProductById = async (id: string) =>
+  await prisma.product.findUnique({ where: { id } });
+
+export const createProduct = async (data: any) =>
+  await prisma.product.create({ data });
+
+export const updateProduct = async (id: string, data: any) =>
+  await prisma.product.update({ where: { id }, data });
+
+export const deleteProduct = async (id: string) =>
+  await prisma.product.delete({ where: { id } });
