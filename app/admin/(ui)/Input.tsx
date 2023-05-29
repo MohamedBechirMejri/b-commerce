24c65376cd4268d00475f030e1ab6f0c707ec123
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-29 17:50:38 +0100

    Add Input Component

---
+++ app/admin/(ui)/Input.tsx
@@ -0,0 +1,26 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+export default function Input({ label = "", ...props }) {
+  const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <motion.div
+      className="relative max-w-xl overflow-hidden rounded-md shadow-sm"
+      initial={{ border: "1px solid #303030" }}
+      animate={{
+        border: isFocused ? "1px solid #6d28d9" : "1px solid #303030",
+      }}
+      transition={{ duration: 0.1 }}
+    >
+      <label className="absolute text-xs font-bold top-4 left-3">{label}</label>
+
+      <motion.input
+        className="relative z-10 w-full h-full px-3 py-4 text-gray-400 bg-transparent border-none outline-none pt-9"
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        {...props}
+      />
+    </motion.div>
+  );
+}
