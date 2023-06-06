74984ea56d360b2af7b6e6a54c92c52ac9a7b043
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-06-06 08:52:26 +0100

    improve nav look

--- app/admin/products/(product)/Nav.tsx
+++ app/admin/products/(product)/Nav.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 const tabs = [
   "details",
   "pricing",
@@ -20,17 +22,33 @@ export default function Nav({
   switchTab: (tab: string) => void;
 }) {
   return (
-    <nav className="flex justify-center w-full h-12 overflow-x-scroll">
+    <nav className="flex justify-center w-full h-12 gap-4 overflow-x-scroll">
       {tabs.map(tab => (
-        <button
+        <div
           key={tab}
-          className={`flex w-32 items-center justify-center text-sm font-semibold uppercase tracking-wide text-gray-500 hover:text-gray-600 ${
-            currentTab === tab ? "text-gray-700" : ""
-          }`}
+          className="flex items-center justify-center"
           onClick={() => switchTab(tab)}
         >
-          {tab}
-        </button>
+          <motion.button
+            initial={{ opacity: 0, y: 10, backgroundColor: "#6d28d922" }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              backgroundColor: currentTab === tab ? "#6d28d9" : "#6d28d922",
+              color: currentTab === tab ? "#fff" : "#6d28d9",
+            }}
+            whileHover={{
+              backgroundColor: currentTab === tab ? "#6d28d9" : "#6d28d933",
+            }}
+            whileTap={{
+              backgroundColor: currentTab === tab ? "#6d28d9" : "#6d28d944",
+            }}
+            transition={{ duration: 0.2 }}
+            className="p-3 text-sm font-semibold tracking-wide uppercase rounded-md"
+          >
+            {tab}
+          </motion.button>
+        </div>
       ))}
     </nav>
   );
