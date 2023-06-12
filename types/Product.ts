a9aafb46ddd3fae108c47f15c3cfab3a6a19d777
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-06-12 21:16:38 +0100

    use zod for product types

--- types/Product.ts
+++ types/Product.ts
@@ -1,17 +1,23 @@
-export type Product = {
-  name: string;
-  reference: string;
-  description: string;
-  categories: string[];
-  brand: string;
-  tags: string;
-  isPublished: boolean;
-  price: number;
-  onSale: boolean;
-  salePrice?: number;
-  stock: number;
-  restockAlert: number;
-  minBuy: number;
-  maxBuy: number;
-  images: string[];
-};
+import { z } from "zod";
+
+const Product = z.object({
+  name: z.string(),
+  reference: z.string(),
+  description: z.string(),
+  categories: z.array(z.string()),
+  brand: z.string(),
+  tags: z.string(),
+  isPublished: z.boolean(),
+  price: z.number(),
+  onSale: z.boolean(),
+  salePrice: z.number().optional(),
+  stock: z.number(),
+  restockAlert: z.number(),
+  minBuy: z.number(),
+  maxBuy: z.number(),
+  images: z.array(z.string()),
+});
+
+export type Product = z.infer<typeof Product>;
+
+export default Product;
