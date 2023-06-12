1d9eb6ccea0597c21bc62b1cb64bb81c20f3b6af
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-06-12 23:40:04 +0100

    remove unused import

--- app/admin/_ui/Select.tsx
+++ app/admin/_ui/Select.tsx
@@ -1,5 +1,5 @@
 import { AnimatePresence, motion } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { TbArrowNarrowDown } from "react-icons/tb";
 
 export default function Select({
