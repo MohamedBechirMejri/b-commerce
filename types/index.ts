184bc7e5a8a2e79ea5b1b0c993e9f998199ff36d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-06-12 23:43:55 +0100

    extend zod Product schema

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,15 @@
-import { Product as P } from "./zod";
+import { z } from "zod";
+
+import { Product as P, ProductSchema } from "./zod";
 
 type Product = P & {
   categories: string[];
   images: string[];
 };
+
+export const ZProduct = ProductSchema.extend({
+  categories: z.array(z.string()),
+  images: z.array(z.string()),
+});
 
 export type { Product };
