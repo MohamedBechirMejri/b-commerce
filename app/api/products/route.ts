401720ac59195bab793511bde0bdc77a83b03ccb
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-06-12 06:39:20 +0100

    add post method

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,9 +1,18 @@
 import { NextResponse } from "next/server";
 
 import prisma from "~/lib/db/prisma";
+import { createProduct } from "~/lib/db/product";
 
 export async function GET() {
   const data = await prisma.product.findMany();
+
+  return NextResponse.json({ data });
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  const data = await createProduct(body);
 
   return NextResponse.json({ data });
 }
--- lib/db/product.ts
+++ lib/db/product.ts
@@ -7,8 +7,37 @@ export const getAllProducts = async () => await prisma.product.findMany();
 export const getProductById = async (id: string) =>
   await prisma.product.findUnique({ where: { id } });
 
-export const createProduct = async (data: Product) =>
-  await prisma.product.create({ data });
+export const createProduct = async (body: any) => {
+  const {
+    name,
+    description,
+    reference,
+    categories,
+    isPublished,
+    images,
+    tags,
+    price,
+    stock,
+    brandId,
+    // authorId,
+  } = body;
+
+  return await prisma.product.create({
+    data: {
+      name,
+      description,
+      reference,
+      isPublished,
+      images: { create: images },
+      tags,
+      price,
+      stock,
+      brand: { connect: { id: brandId } },
+      // author: { connect: { id: authorId } },
+      categories: { connect: categories.map((id: string) => ({ id })) },
+    },
+  });
+};
 
 export const updateProduct = async (id: string, data: Product) =>
   await prisma.product.update({ where: { id }, data });
