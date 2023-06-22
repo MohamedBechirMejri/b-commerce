333762adccfd281c7d8bc254078da30d56dd5769
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-06-22 09:40:29 +0100

    add todo

--- app/admin/products/[id]/page.tsx
+++ app/admin/products/[id]/page.tsx
@@ -1,5 +1,7 @@
 import Product from "../Product";
 
+// TODO: use ssr to get the product data and add loadinc page
+
 export default function ({ params }: { params: { id: string } }) {
   return (
     <div className="grid h-full grid-rows-[auto,1fr]">
