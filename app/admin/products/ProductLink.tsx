93bea48d97dd389b1f09b0b879dc355bae3e01c3
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-07-03 00:37:21 +0100

    fix typo

--- app/admin/products/ProductLink.tsx
+++ app/admin/products/ProductLink.tsx
@@ -18,7 +18,7 @@ export default function ProductLink({ product }: { product: Product }) {
       <td className="px-4 py-2">
         <Image
           src={
-            product.images
+            product.images[0]
               ? product.images[0]
               : "https://via.placeholder.com/150"
           }
