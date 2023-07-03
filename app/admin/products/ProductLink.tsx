f68ae0d0b3db073df739181de0b2ac18a6b740e3
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-07-03 08:11:05 +0100

    fit images

--- app/admin/products/ProductLink.tsx
+++ app/admin/products/ProductLink.tsx
@@ -32,7 +32,7 @@ export default function ProductLink({ product }: { product: Product }) {
           alt={product.name}
           width={40}
           height={150}
-          className="rounded-md"
+          className="rounded-md w-auto h-auto"
         />
       </td>
       <td className="px-4 py-2">{product.name}</td>
