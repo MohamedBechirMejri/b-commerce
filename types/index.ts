074de91832049fa4e83acb6a0ac0d1abf39e7073
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-07-03 08:54:08 +0100

    verify strings lengths

--- types/index.ts
+++ types/index.ts
@@ -10,6 +10,9 @@ export const ZProduct = ProductSchema.extend({
   id: z.string().optional(),
   createdAt: z.coerce.date().optional(),
   updatedAt: z.coerce.date().optional(),
+
+  name: z.string().min(5).max(72),
+  reference: z.string().min(3).max(255),
 });
 
 export type { Product };
