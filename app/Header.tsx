3cc45084560b25fa0f67519f1b1d75ba26edc1f3
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-07-05 15:20:33 +0100

    add header boilerplate

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,6 +3,7 @@
 import { usePathname } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
+import { TbSearch, TbShoppingCart, TbUser } from "react-icons/tb";
 
 export default function Header() {
   const pathname = usePathname();
@@ -10,8 +11,19 @@ export default function Header() {
   return pathname.split("/")[1] === "admin" ? (
     <></>
   ) : (
-    <header className="h-[4rem] w-full bg-rose-500 flex justify-between items-center p-4">
-      <div className="flex gap-8">
+    <header className="h-[4rem] w-full bg-[#aea8a1] grid grid-cols-3 p-4 place-items-center text-white">
+      <nav className="w-full">
+        <ul className="flex gap-4">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Link href={"/"}>
         <Image
           src={"/logo.svg"}
           alt="Logo"
@@ -19,16 +31,18 @@ export default function Header() {
           height={50}
           className="w-32"
         />
-        <nav>
-          <ul className="flex gap-4">
-            <li>
-              <Link href="/products">Products</Link>
-            </li>
-            <li>
-              <Link href="/cart">Cart</Link>
-            </li>
-          </ul>
-        </nav>
+      </Link>
+
+      <div className="w-full flex justify-end text-2xl gap-6">
+        <button>
+          <TbSearch />
+        </button>
+        <button>
+          <TbShoppingCart />
+        </button>
+        <button>
+          <TbUser />
+        </button>
       </div>
     </header>
   );
