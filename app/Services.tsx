137152d6f2da15f2c5b13750e62d95f6d86a703d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-07-19 09:56:05 +0100

    tweak spacing

--- app/Services.tsx
+++ app/Services.tsx
@@ -4,26 +4,25 @@ import { TbCreditCard, TbMoodHappy, TbTruckDelivery } from "react-icons/tb";
 
 export default function Services() {
   return (
-    <section className="mx-auto py-8 w-ful flex flex-col items-center overflow-hidden bg-yellow-600 text-white">
+    <section className="mx-auto py-8 w-ful flex flex-col items-center overflow-hidden bg-yellow-600 text-white gap-8">
       <h1 className="font-bold text-3xl">Our Services and Garantees</h1>
-      <p className="text-lg text-opacity-70">Our Latest Additions</p>
 
-      <div className="pt-8 flex gap-8 text-5xl text-center font-semibold">
-        <div className="flex flex-col items-center gap-4">
+      <div className="flex gap-8 text-4xl text-center font-semibold">
+        <div className="flex flex-col items-center gap-2">
           <TbTruckDelivery />
-          <p className="text-lg">Deliveries in 48h</p>
+          <p className="text-sm">Deliveries in 48h</p>
         </div>
-        <div className="flex flex-col items-center gap-4">
+        <div className="flex flex-col items-center gap-2">
           <TbMoodHappy />
-          <p className="text-lg">
-            Your satisfaction
+          <p className="text-sm">
+            Your satisfaction is
             <br />
-            is our goal
+            our Goal!
           </p>
         </div>
-        <div className="flex flex-col items-center gap-4">
+        <div className="flex flex-col items-center gap-2">
           <TbCreditCard />
-          <p className="text-lg">Secure payments</p>
+          <p className="text-sm">Secure payments</p>
         </div>
       </div>
     </section>
