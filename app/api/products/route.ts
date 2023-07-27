aaa2eb03668aa0825d018220f0119c40e7628403
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-07-27 09:52:48 +0100

    add filter examples

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -4,14 +4,32 @@ import prisma from "~/lib/db/prisma";
 import { createProduct } from "~/lib/db/product";
 
 export async function GET({ nextUrl }: NextRequest) {
-  if (nextUrl.searchParams.get("latest")) {
+  const getParam = (key: string) => nextUrl.searchParams.get(key);
+
+  if (getParam("latest")) {
     const data = await prisma.product.findMany({
       orderBy: { createdAt: "desc" },
       take: 10,
     });
-
     return NextResponse.json({ data });
   }
+
+  // if (getParam("top_rated")) {
+  //   const data = await prisma.product.findMany({
+  //     orderBy: { rating: "desc" },
+  //     take: 10,
+  //   });
+  //   return NextResponse.json({ data });
+  // }
+
+  // if (getParam("best_sellers")) {
+  //   const data = await prisma.product.findMany({
+  //     orderBy: { sold: "desc" },
+  //     take: 10,
+  //   });
+  //   return NextResponse.json({ data });
+  // }
+
   const data = await prisma.product.findMany();
 
   return NextResponse.json({ data });
