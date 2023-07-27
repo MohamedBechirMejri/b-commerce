04d14a2f5982518b5c318597a2ce58ec7a87f78b
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-07-27 21:21:18 +0100

    add filters prop

--- lib/hooks/useProducts.ts
+++ lib/hooks/useProducts.ts
@@ -1,13 +1,19 @@
 import { useState, useEffect } from "react";
 
-export default function useProducts(filters: string) {
+export default function useProducts(filters: {
+  [key: string]: string | string[] | boolean | number;
+}) {
   const [products, setProducts] = useState([]) as any[];
+
+  const parsedFilters = Object.entries(filters)
+    .map(f => f.join("="))
+    .join("&");
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const { data } = await fetch(`/api/products?${filters}`).then(res =>
-          res.json()
+        const { data } = await fetch(`/api/products?${parsedFilters}`).then(
+          res => res.json()
         );
         setProducts(data);
       } catch (error) {
@@ -18,5 +24,5 @@ export default function useProducts(filters: string) {
     fetchProducts();
   }, []);
 
-  return { products };
+  return [...products, ...products, ...products, ...products];
 }
