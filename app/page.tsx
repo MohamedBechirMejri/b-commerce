f956fcf1bc23ce278196857409905b536deedc62
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-07-30 05:36:03 +0100

    Add coupons section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import dynamic from "next/dynamic";
 
 import Hero from "./Hero";
 import Products from "./(Products)";
+import Coupons from "./Coupons";
 
 const Categories = dynamic(() => import("./Categories"), { ssr: false });
 
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero />
       <Categories />
       <Products />
+      <Coupons />
       <div className="h-screen"></div> {/* TODO: remove this */}
     </main>
   );
