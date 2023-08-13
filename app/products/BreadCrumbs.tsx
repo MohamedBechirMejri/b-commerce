e48fd3254c75141a897591b57f273d2daa1adadb
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-08-13 11:51:02 +0100

    Add BreadCrumbs

---
+++ app/products/BreadCrumbs.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { TbHome } from "react-icons/tb";
+
+const BreadCrumbs = () => {
+  const pathname = usePathname();
+  const paths = pathname.split("/");
+
+  return (
+    <div className="flex items-center gap-1 text-sm text-gray-500 w-full max-w-7xl mx-auto py-4 capitalize">
+      {paths.map((path, i) => {
+        const isLast = i === paths.length - 1;
+        const link = paths.slice(0, i + 1).join("/") || "/";
+
+        if (isLast) return <span key={"breadcrumblink" + i}>{path}</span>;
+
+        return (
+          <Link
+            href={link}
+            key={"breadcrumblink" + i}
+            className="flex items-center gap-1"
+          >
+            <div className="hover:underline">
+              {path || (
+                <span className="flex gap-1 items-center">
+                  <TbHome className="-mt-0.5" /> {"Home"}
+                </span>
+              )}
+            </div>
+            <span>/</span>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default BreadCrumbs;
---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+import BreadCrumbs from "./BreadCrumbs";
+
+export default function Products() {
+  return (
+    <div className="">
+      <BreadCrumbs />
+    </div>
+  );
+}
